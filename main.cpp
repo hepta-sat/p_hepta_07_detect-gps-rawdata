075f62908d7281faea02ae805f5997942fcb6d06
sonenagisa <csna20061@g.nihon-u.ac.jp>
2024-06-28 02:03:23 +0000

    first commit

--- main.cpp
+++ main.cpp
@@ -1,12 +1,18 @@
 #include "mbed.h"
+#include "HEPTA_EPS.h"
+#include "HEPTA_CDH.h"
+#include "HEPTA_SENSOR.h"
 
-DigitalOut myled(LED1);
+RawSerial pc(USBTX,USBRX);
+HEPTA_EPS eps(p16,p26);
+HEPTA_CDH cdh(p5,p6,p7,p8,"sd");
+HEPTA_SENSOR sensor(p17,
+                  p28,p27,0x19,0x69,0x13,
+                  p13, p14,p25,p24);
 
-int main() {
-  while(1) {
-    myled = 1;
-    wait(0.2);
-    myled = 0;
-    wait(0.2);
-  }
+int main(){
+    sensor.gps_setting();
+    pc.printf("GPS Raw Data Mode\r\n");
+    while(1) pc.putc(sensor.getc());
 }
+
